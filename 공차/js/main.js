839b7e655f7ddbd2e248ce32c773cb59048ae455
[msms603179] <[sgudfhrs@naver.com]>
2022-07-02 03:13:43 +0900

    포트폴리오

--- 공차/js/main.js
+++ 공차/js/main.js
@@ -35,5 +35,66 @@ $(document).ready(function (){
   $('.kakao').mouseout(function(){
     $(this).attr('src', 'img/kakao.png');
   })
-}) 
 
+
+
+
+
+  $(".tea").on("click", function(){
+
+  });
+
+  $(".tea").click(function(){
+    $(".tea_img").attr('src', 'img/tea.png');
+    $(".milktea_img").attr('src', 'img/milkbl.png');
+    $(".fruit_img").attr('src', 'img/fruitbl.png');
+    $(".jewelry_img").attr('src', 'img/jewelrybl.png');
+    $("#section02 .inner").css("height","687px");
+
+    $(".tea_list").fadeIn();
+    $(".milktea_list").fadeOut();
+    $(".fruitmix_list").fadeOut();
+    $(".jewelry_list").fadeOut();
+  });
+
+  $(".milktea").click(function(){
+    $(".tea_img").attr('src', 'img/teabl.png');
+    $(".milktea_img").attr('src', 'img/milk.png');
+    $(".fruit_img").attr('src', 'img/fruitbl.png');
+    $(".jewelry_img").attr('src', 'img/jewelrybl.png');
+    $("#section02 .inner").css("height","1860px");
+
+    $(".tea_list").fadeOut();
+    $(".milktea_list").fadeIn();
+    $(".fruitmix_list").fadeOut();
+    $(".jewelry_list").fadeOut();
+  });
+
+  $(".fruitmix").click(function(){
+    $(".tea_img").attr('src', 'img/teabl.png');
+    $(".milktea_img").attr('src', 'img/milkbl.png');
+    $(".fruit_img").attr('src', 'img/fruit.png');
+    $(".jewelry_img").attr('src', 'img/jewelrybl.png');
+    $("#section02 .inner").css("height","1239px");
+    $(".tea_list").fadeOut();
+    $(".milktea_list").fadeOut();
+    $(".fruitmix_list").fadeIn();
+    $(".jewelry_list").fadeOut();
+  });
+
+  $(".jewelry").click(function(){
+    $(".tea_img").attr('src', 'img/teabl.png');
+    $(".milktea_img").attr('src', 'img/milkbl.png');
+    $(".fruit_img").attr('src', 'img/fruitbl.png');
+    $(".jewelry_img").attr('src', 'img/jewelry.png');
+    $("#section02 .inner").css("height","687px");
+    $(".tea_list").fadeOut();
+    $(".milktea_list").fadeOut();
+    $(".fruitmix_list").fadeOut();
+    $(".jewelry_list").fadeIn();
+  });
+
+}) 
+$('#item2 .fadeIn').on('click', function() {
+  $('#item2 .target-box').fadeIn()
+})
